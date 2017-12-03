7ea30cfdb3e4297a8af8a9d9c0a3f7c3fb502bc1
caoru828 <18846432589@163.com>
2017-12-03 13:55:01 +0800

    vuecli的app练习

--- src/components/movie/MovieList.vue
+++ src/components/movie/MovieList.vue
@@ -1,16 +1,36 @@
 <template>
   <div class="List">
-    <h2>List</h2>
+    <ul>
+      <!--key便与虚拟dom查找，-->
+      <li v-for="(movie) in movieList" :key="movie.id"></li>
+      <img :src="movie.img" alt="">
+      </ul>
   </div>
 </template>
 
 <script>
-
-
+//  这个可以在package.json里找到axios
+import Axios from 'axios'
+Axios.get
 
   export default {
+  data(){
+    return{
+      movieList:[]
+    }
+  },
     components: {
 
+    },
+//    加载完成时
+    mounted(){
+
+       Axios.get(API_PROXY +"http://m.maoyan.com/movie/list.json?type=hot&limit=10&offset=0")
+         .then((res)=>{
+         console.log(res);
+         this.movieList = res.data.data.movies;
+       })
+         .catch(()=>{});
     }
   }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,8 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: HelloWorld,
+      redirect:'/movie/movieList'
     },{
       path:'/movie',
       component:Movie,
