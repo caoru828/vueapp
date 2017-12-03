95ff771093bc3777c381c692dc1a5bebb1ed4c61
caoru828 <18846432589@163.com>
2017-12-03 11:00:27 +0800

    vuecli的app练习

---
+++ src/components/common/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ul class="footer">
+      <li>电影</li>
+      <li>音乐</li>
+      <li>书籍</li>
+      <li>图片</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'footer',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.footer{
+  position: fixed;
+  bottom: 0;
+  height: 1rem;
+  width: 100%;
+  background: rgb(33,150,243);
+  display: flex;
+}
+.footer li{
+  flex-grow: 1;
+  color: white;
+  line-height: 1rem;
+  text-align: center;
+}
+</style>
---
+++ src/components/common/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header">
+    <button class="btn">首页</button>
+    <h2 class="title">Movie</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'header',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.header {
+  height: 1rem;
+  background: rgb(33, 150, 243);
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+.btn{
+  background: rgb(33,150,243);
+  border:0;
+  padding-top:0 ;
+  padding-bottom: 0;
+  line-height: 1rem;
+  color: white;
+
+}
+.title{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  color: white;
+}
+  /*position: absolute;*/
+  /*top:50%;*/
+  /*left: 50%;*/
+
+
+
+</style>
