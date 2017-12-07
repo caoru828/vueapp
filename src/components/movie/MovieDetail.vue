812d022e3c8394a20268f1840b107e3f74a1c046
caoru828 <18846432589@163.com>
2017-12-07 09:41:07 +0800

    vuecli的app练习

--- src/components/movie/MovieDetail.vue
+++ src/components/movie/MovieDetail.vue
@@ -1,14 +1,45 @@
 <template>
   <div class="detail">
-    {{$route.params.movieID}}
+    <!--{{$route.params.movieID}}-->
+    <div>
+      <img :src="this.imgSrc" alt="">
+    </div>
+    <div class="MovieDetail">
+     <div>{{this.name}}</div>
+      <p>{{this.star}}</p>
+      {{this.Info}}
+    </div>
+
+
   </div>
 </template>
 
 <script>
-
-
-
+  import Axios from 'axios'
   export default {
+    data(){
+      return{
+        name:[],
+        imgSrc:"",
+        star:'',
+        Info:''
+      }
+    },
+    mounted(){
+      let URL3 = API_PROXY+
+        "http://m.maoyan.com/movie/"+
+        this.$route.params.movieID +
+        ".json";
+
+      Axios.get(URL3)
+      .then(res=>{
+        this.name=res.data.data.MovieDetailModel.nm;
+        this.imgSrc = res.data.data.MovieDetailModel.img;
+        this.star = res.data.data.MovieDetailModel.dir;
+        this.Info = res.data.data.MovieDetailModel.dra;
+        console.log(res);
+      }).catch()
+    },
     components: {
 
     }
