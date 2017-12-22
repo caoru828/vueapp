95d3b86dcf70f31e104a529cb5c65ad150d30e90
caoru828 <18846432589@163.com>
2017-12-22 17:44:51 +0800

    vuecli的app练习

--- src/components/photos/PhotosDetail.vue
+++ src/components/photos/PhotosDetail.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="photos">
 
-
   </div>
 </template>
 <script>
-
 
 
 
--- src/components/photos/PhotosList.vue
+++ src/components/photos/PhotosList.vue
@@ -1,13 +1,44 @@
 <template>
   <div class="photos">
 
+    <ul>
+      <li v-for="(photo,index) in photoList" :key="index">
+        <img :src="photo.src" alt="">
+      </li>
+    </ul>
+
   </div>
 </template>
 <script>
 
+import Axios from 'axios'
+export default {
+  data(){
+    return{
+      photoList : []
+    }
+  },
+  mounted(){
+//    promise对象
+    Axios.get('/static/data/photodata.json').then(res=>{
+      this.photoList= res.data.photoData;
+    }).catch()
+  }
+}
 
 
 </script>
 <style scoped>
+  .photos{
+    margin-top:1rem;
+    margin-bottom: 1rem;
+  }
+  .photos li{
+    width: 50%;
+    display: inline-block;
+  }
+  .photos img{
+    width: 100%;
+  }
 
 </style>
